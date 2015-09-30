e400a69097a2b56f2c466319b29155191a77bd13
Selina Wang <ziyu.wang@students.olin.edu>
2015-09-30 18:58:01 -0400

    Completed 4 bit adder verilog and do files

--- Lab/Lab0/adder.v
+++ Lab/Lab0/adder.v
@@ -44,9 +44,60 @@ wire [3:0] sum;
 wire carryout, overflow;
 FullAdder4bit adder4bit(sum, carryout, overflow, a, b);
 initial begin
+$display("Test Adder 0 Pos:");
 $display("a     b    | sum    carryout   overflow| Expected Output");
-a=4'b0001;b=4'b0111; #1000
+a=4'b0000;b=4'b0001; #1000
+$display("%b  %b | %b   %b          %b       | 0001    0     0", a, b, sum, carryout,overflow);
+a=4'b0001;b=4'b0001; #1000
+$display("%b  %b | %b   %b          %b       | 0010    0     0", a, b, sum, carryout,overflow);
+$display("Test Adder 0 Neg:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b1111;b=4'b0000; #1000
+$display("%b  %b | %b   %b          %b       | 1111    0     0", a, b, sum, carryout,overflow);
+a=4'b1111;b=4'b1111; #1000
+$display("%b  %b | %b   %b          %b       | 1110    1     0", a, b, sum, carryout,overflow);
+$display("Test Adder 1 Pos:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b0000;b=4'b0010; #1000
+$display("%b  %b | %b   %b          %b       | 0010    0     0", a, b, sum, carryout,overflow);
+a=4'b0010;b=4'b0010; #1000
+$display("%b  %b | %b   %b          %b       | 0100    0     0", a, b, sum, carryout,overflow);
+$display("Test Adder 1 Neg:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b1110;b=4'b0000; #1000
+$display("%b  %b | %b   %b          %b       | 1110    0     0", a, b, sum, carryout,overflow);
+a=4'b1110;b=4'b1110; #1000
+$display("%b  %b | %b   %b          %b       | 1100    1     0", a, b, sum, carryout,overflow);
+$display("Test Adder 2 Pos:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b0000;b=4'b0100; #1000
+$display("%b  %b | %b   %b          %b       | 0100    0     0", a, b, sum, carryout,overflow);
+a=4'b0100;b=4'b0100; #1000
 $display("%b  %b | %b   %b          %b       | 1000    0     1", a, b, sum, carryout,overflow);
+$display("Test Adder 2 Neg:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b1100;b=4'b0000; #1000
+$display("%b  %b | %b   %b          %b       | 1100    0     0", a, b, sum, carryout,overflow);
+a=4'b1100;b=4'b1100; #1000
+$display("%b  %b | %b   %b          %b       | 1000    1     0", a, b, sum, carryout,overflow);
+$display("Test Adder 3 Carryout Pos + Pos:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b0111;b=4'b0000; #1000
+$display("%b  %b | %b   %b          %b       | 0111    0     0", a, b, sum, carryout,overflow);
+a=4'b0111;b=4'b0001; #1000
+$display("%b  %b | %b   %b          %b       | 1000    0     1", a, b, sum, carryout,overflow);
+$display("Test Adder 3 Carryout Neg + Neg:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b0000;b=4'b1000; #1000
+$display("%b  %b | %b   %b          %b       | 1000    0     0", a, b, sum, carryout,overflow);
+a=4'b1000;b=4'b1000; #1000
+$display("%b  %b | %b   %b          %b       | 0000    1     1", a, b, sum, carryout,overflow);
+$display("Test Adder 3 Carryout Neg + Pos:");
+$display("a     b    | sum    carryout   overflow| Expected Output");
+a=4'b1011;b=4'b0110; #1000
+$display("%b  %b | %b   %b          %b       | 0001    1     0", a, b, sum, carryout,overflow);
+a=4'b0010;b=4'b1100; #1000
+$display("%b  %b | %b   %b          %b       | 1110    0     0", a, b, sum, carryout,overflow);
 end
 endmodule
 
