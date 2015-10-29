08e04ac643c3b2bcbe300d37b4841184100ffda1
William Saulnier <lego6245@gmail.com>
2015-10-29 14:01:22 -0400

    yay shiftregister

--- Lab/Lab2/shiftregister.v
+++ Lab/Lab2/shiftregister.v
@@ -14,12 +14,20 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg[width-1:0] parallelDataOut,    // Shift reg data contents
+output reg          serialDataOut       // Positive edge synchronized
 );
 
     reg [width-1:0]      shiftregistermem;
     always @(posedge clk) begin
-        // Your Code Here
+      if (peripheralClkEdge) begin
+        shiftregistermem <= shiftregistermem << 1;
+        shiftregistermem[0] <= serialDataIn;
+        serialDataOut <= shiftregistermem[width-1];
+      end
+      if (parallelLoad) begin
+        shiftregistermem <= parallelDataIn;
+      end
+      parallelDataOut <= shiftregistermem;
     end
 endmodule
