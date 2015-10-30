9f6d37ff4c4055dda88900cef6b917e065191dec
BrennaManning <brenna.manning@students.olin.edu>
2015-10-30 10:41:04 -0400

    input conditioner test benches

--- Lab/Lab2/inputconditioner.t.v
+++ Lab/Lab2/inputconditioner.t.v
@@ -9,14 +9,14 @@ module testConditioner();
     wire conditioned;
     wire rising;
     wire falling;
-    
+    wire dutpassed;
     inputconditioner dut(.clk(clk),
     			 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
 			 .negativeedge(falling))
 
-
+	
     // Generate clock (50MHz)
     initial clk=0;
     always #10 clk=!clk;    // 50MHz Clock
@@ -25,5 +25,39 @@ module testConditioner();
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
     // Synchronize, Clean, Preprocess (edge finding)
-    
+
+
+ //Synchronize: 5 seconds delay
+	pin = 1;
+	for(clk=0; clk<4; clk=clk+1) begin
+		if(rising || falling)begin
+			dutpassed = 0;
+			$display("Synchronize Failed: Edge detection before expected delay");
+		end
+	end
+	
+
+//Clean: Bounce up to 5 clock cycles - remain the same
+	pin = 1;
+	integer i = 0;
+	for(i=0; i<5; i = i +1)begin 
+		clk = i;
+		pin = i%2;
+		if(rising || falling)begin
+			dutpassed = 0;
+			$display("Clean Failed: Edge detection from bouncing");
+		end
+	end
+	
+		
+		
+		
+
+//Preprocess: when edge should change we detect both edges
+	pin = 1;
+	if ((clk = 4) && (!rising && !falling)) begin
+		dutpassed =0;
+		$display("Preprocess Failed: No edge detection when expected");
+	end
+
 endmodule
