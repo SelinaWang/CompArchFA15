fed37cca7c51dd84b31eb16f1e4dd89341cbee66
SelinaW <selina@audiocommon.com>
2015-11-09 15:52:50 -0500

    breakable address latch

--- Lab/Lab2/addresslatch.v
+++ Lab/Lab2/addresslatch.v
@@ -14,6 +14,27 @@ input                   fault_pin
     always @(posedge clk) begin
         if (ce) begin
             q <= d;
+        end
+    end
+
+endmodule
+
+// 8-bit Address Latch with enable and fault injection
+//   Positive edge triggered
+module addresslatch_breakable
+(
+output reg[7:0]		q,
+input[7:0]		d,
+input			ce,
+input			clk,
+input                   fault_pin
+);
+    initial begin
+      q <= 0;
+    end
+    always @(posedge clk) begin
+        if (ce) begin
+            q <= d;
             if(fault_pin) begin
               q[7] <= 1;
             end
