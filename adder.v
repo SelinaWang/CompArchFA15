f9689d56468dcac5b3cfe70ec9b6cb60679f15bc
SelinaW <selina@audiocommon.com>
2015-12-12 17:54:12 -0500

    Encryptor working

---
+++ adder.v
@@ -0,0 +1,10 @@
+module FullAdder4bit
+(
+  output[3:0] sum,  // 2's complement sum of a and b
+  output carryout,  // Carry out of the summation of a and b
+  output overflow,  // True if the calculation resulted in an overflow
+  input[3:0] a,     // First operand in 2's complement format
+  input[3:0] b      // Second operand in 2's complement format
+);
+    // Your Code Here
+endmodule
